074e2e1163c2f016ffdbac7a22d7c31b0a279017
Stanislaw Cruz <stanycruz@outlook.com>
2024-01-17 19:59:39 -0300

    configurando typeorm com postgres

---
+++ src/database/database.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '82vu7jhw',
+  database: 'devtraining',
+  entities: [],
+  synchronize: true,
+};
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        return {
+          ...dataSourceOptions,
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
