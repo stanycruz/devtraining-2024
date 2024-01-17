88d0f68045cd8811102733f258c00957089d6c94
Stanislaw Cruz <stanycruz@outlook.com>
2024-01-17 10:32:41 -0300

    customizando status code

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
-  findAll() {
-    return 'Listagem de cursos';
+  findAll(@Res() response) {
+    return response.status(200).json({ message: 'Listagem de cursos' });
   }
 
   @Get(':id')
