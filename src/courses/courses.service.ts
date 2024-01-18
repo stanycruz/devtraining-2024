1aa053382ed0751b0d3a6d590a60da46caf0956c
Stanislaw Cruz <stanycruz@outlook.com>
2024-01-18 11:47:15 -0300

    refatorando create e update com relacionamentos

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -2,12 +2,17 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Course } from './entities/courses.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Tag } from './entities/tags.entity';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/update-course.dto';
 
 @Injectable()
 export class CoursesService {
   constructor(
     @InjectRepository(Course)
     private readonly courseRepository: Repository<Course>,
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
   ) {}
 
   async findAll() {
@@ -26,15 +31,27 @@ export class CoursesService {
     return course;
   }
 
-  async create(createCourseDTO: any) {
-    const course = this.courseRepository.create(createCourseDTO);
+  async create(createCourseDTO: CreateCourseDTO) {
+    const tags = await Promise.all(
+      createCourseDTO.tags.map((name) => this.preloadTagByName(name)),
+    );
+    const course = this.courseRepository.create({
+      ...createCourseDTO,
+      tags,
+    });
     return await this.courseRepository.save(course);
   }
 
-  async update(id: number, updateCourseDTO: any) {
+  async update(id: number, updateCourseDTO: UpdateCourseDTO) {
+    const tags =
+      updateCourseDTO.tags &&
+      (await Promise.all(
+        updateCourseDTO.tags.map((name) => this.preloadTagByName(name)),
+      ));
     const course = await this.courseRepository.preload({
       ...updateCourseDTO,
       id,
+      tags,
     });
 
     if (!course) {
@@ -54,5 +71,19 @@ export class CoursesService {
     }
 
     return await this.courseRepository.remove(course);
+  }
+
+  private async preloadTagByName(name: string): Promise<Tag> {
+    const tag = await this.tagRepository.findOne({
+      where: {
+        name,
+      },
+    });
+
+    if (tag) {
+      return tag;
+    }
+
+    return this.tagRepository.create({ name });
   }
 }
