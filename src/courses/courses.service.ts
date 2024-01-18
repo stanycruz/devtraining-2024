6b9649bad3ef5c944f661eb40fd4aa644ac8a31b
Stanislaw Cruz <stanycruz@outlook.com>
2024-01-18 12:04:34 -0300

    refatorando findAll e findOne

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -16,12 +16,15 @@ export class CoursesService {
   ) {}
 
   async findAll() {
-    return await this.courseRepository.find();
+    return await this.courseRepository.find({
+      relations: ['tags'],
+    });
   }
 
   async findOne(id: number) {
     const course = await this.courseRepository.findOne({
       where: { id },
+      relations: ['tags'],
     });
 
     if (!course) {
