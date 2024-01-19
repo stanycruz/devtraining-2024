e147b6dbdc792a6f8a07412c5865602cd21ac52b
Stanislaw Cruz <stanycruz@outlook.com>
2024-01-19 12:42:55 -0300

    criando a tabela courses_tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1705667258873 } from 'src/migrations/1705667258873-CreateCoursesTable';
 import { CreateTagsTable1705672317864 } from 'src/migrations/1705672317864-CreateTagsTable';
+import { CreateCoursesTagsTable1705677127141 } from 'src/migrations/1705677127141-CreateCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1705667258873, CreateTagsTable1705672317864],
+  migrations: [
+    CreateCoursesTable1705667258873,
+    CreateTagsTable1705672317864,
+    CreateCoursesTagsTable1705677127141,
+  ],
 });
