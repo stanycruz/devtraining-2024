6b50b5269e8fe2365473dd4df92bfe0db20e68d8
Stanislaw Cruz <stanycruz@outlook.com>
2024-01-19 12:57:19 -0300

    criando a coluna coursesId na tabela courses_tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1705667258873 } from 'src/migrations/1705667258873-CreateCoursesTable';
 import { CreateTagsTable1705672317864 } from 'src/migrations/1705672317864-CreateTagsTable';
 import { CreateCoursesTagsTable1705677127141 } from 'src/migrations/1705677127141-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1705679116291 } from 'src/migrations/1705679116291-AddCoursesIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +12,6 @@ export const dataSource = new DataSource({
     CreateCoursesTable1705667258873,
     CreateTagsTable1705672317864,
     CreateCoursesTagsTable1705677127141,
+    AddCoursesIdToCoursesTagsTable1705679116291,
   ],
 });
